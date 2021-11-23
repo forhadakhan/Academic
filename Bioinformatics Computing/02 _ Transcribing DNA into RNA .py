ac25c2b8a10e00777640e4a819e6099e1787c81d
Forhad Ahmed Khan <67508944+forhadkhan@users.noreply.github.com>
2021-11-23 16:10:03 +0600

    Updated Solution 02

--- Bioinformatics Computing/02 _ Transcribing DNA into RNA .py
+++ Bioinformatics Computing/02 _ Transcribing DNA into RNA .py
@@ -1,7 +1,7 @@
 '''
   (http://rosalind.info/problems/rna/)
   
-  Problem
+  Problem:
           An RNA string is a string formed from the alphabet containing 'A', 'C', 'G', and 'U'.
 
           Given a DNA string t corresponding to a coding strand, its transcribed RNA string u is formed by replacing all occurrences of 'T' in t with 'U' in u.
