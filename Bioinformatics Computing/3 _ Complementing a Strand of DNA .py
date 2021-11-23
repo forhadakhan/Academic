aa24220b1d20e22a6f89252e0716db2207124a00
Forhad Ahmed Khan <67508944+forhadkhan@users.noreply.github.com>
2021-11-23 16:05:21 +0600

    Updated Solution for Problem 03

--- Bioinformatics Computing/3 _ Complementing a Strand of DNA .py
+++ Bioinformatics Computing/3 _ Complementing a Strand of DNA .py
@@ -4,13 +4,14 @@
   Problem
           In DNA strings, symbols 'A' and 'T' are complements of each other, as are 'C' and 'G'.
 
-          The reverse complement of a DNA string s is the string sc formed by reversing the symbols of s, then taking the complement of each symbol (e.g., the reverse complement of "GTCA" is "TGAC").
+          The reverse complement of a DNA string s is the string sc formed by reversing the symbols of s, 
+          then taking the complement of each symbol (e.g., the reverse complement of "GTCA" is "TGAC").
 
   Given: 
-          A DNA string s of length at most 1000 bp.
+          A DNA string s of length at most 1000 base pairs.
 
   Return: 
-          he reverse complement sc of s.
+          The reverse complement s^c of s.
 
   Sample Dataset:
           AAAACCCGGT
